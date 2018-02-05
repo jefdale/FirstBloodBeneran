b9e85d0fddf346b7ded75e8c69c4a8b8ac253529
jefdale <jefdale@github.com>
2018-02-05 16:34:39 +0700

    UI

--- app/src/main/java/com/example/dale/firstblood/MainActivity.java
+++ app/src/main/java/com/example/dale/firstblood/MainActivity.java
@@ -17,17 +17,17 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_donor:
+                    mTextMessage.setText("Donor");
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_recipient:
+                    mTextMessage.setText("Recipient");
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_tips:
+                    mTextMessage.setText("Tips");
                     return true;
-                case R.id.navigation_faq:
-                    mTextMessage.setText("FAQ");
+                case R.id.navigation_profile:
+                    mTextMessage.setText("Profile");
                     return true;
             }
             return false;
@@ -40,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
---
+++ app/src/main/java/com/example/dale/firstblood/RecipientFragment.java
@@ -0,0 +1,21 @@
+package com.example.dale.firstblood;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Dale on 2/5/2018.
+ */
+
+public class RecipientFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.recipient,container,false);
+        return v;
+    }
+}
